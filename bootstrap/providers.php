a622bbdb3ac43d6496080a8b02a4a7860119b797
Angus <angus.goody@gmail.com>
2026-01-23 12:00:44 +0000

    mailjet

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,9 @@
 <?php
 
+use Mailjet\LaravelMailjet\MailjetServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    MailjetServiceProvider::class,
 ];
