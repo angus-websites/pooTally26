f4f31a108403c7d9eb6e7f8869f19bc30348ff04
Angus <angus.goody@gmail.com>
2026-01-23 14:49:20 +0000

    added canonical

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -16,6 +16,7 @@
 <meta name="apple-mobile-web-app-title" content="PooTally"/>
 <link rel="manifest" href="/assets/images/core/site.webmanifest"/>
 
+<link rel="canonical" href="{{ request()->fullUrl() }}">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
