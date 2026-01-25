15e2df58348de8796208c8ce60d92bfac16daa75
Angus <angus.goody@gmail.com>
2026-01-25 14:34:52 +0000

    new login

---
+++ resources/views/components/public/header-minimal.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="">
+        <div
+            class="mx-auto flex max-w-7xl items-center justify-center p-6 md:space-x-10 px-8">
+            <div class="flex justify-center lg:w-0 lg:flex-1">
+                <a href="{{ route('home') }}" class="flex items-center justify-center" wire:navigate>
+                    <x-app-logo/>
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+
